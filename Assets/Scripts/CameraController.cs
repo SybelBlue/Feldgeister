74c8a829c29dc7ea0be00d96931bee5aae02c088
L Caraco <lbc.dna@gmail.com>
2020-11-15 16:32:05 -0600

    added zoom

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -13,13 +13,31 @@ public class CameraController : MonoBehaviour
     [SerializeField, Range(1, 60), Tooltip("Number of frames that pass before checking to pan with mouse. (Higher value = Slower pan).")]
     private int framesPerPan = 10;
 
+    [SerializeField, Range(0, 1)]
+    private float scrollSensitivity = 0.1f;
+
+    [SerializeField, ReadOnly]
+    private float zoomValue = 1;
+
+    [SerializeField]
+    private float minZoomValue, maxZoomValue;
+
+    private Vector3 originalScale;
+
+    private void Start()
+    {
+        originalScale = transform.localScale;
+    }
+
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (Time.frameCount % framesPerPan == 0)
         {
             PanWithMouse();
         }
+
+        UpdateZoom();
     }
 
     private void PanWithMouse()
@@ -44,6 +62,18 @@ public class CameraController : MonoBehaviour
         {
             transform.position += pctMouseVwPos.y < 50 ? Vector3.down : Vector3.up;
         }
+    }
+
+    private void UpdateZoom()
+    {
+        float scroll = Input.mouseScrollDelta.y;
+        
+        if (scroll == 0) return;
+
+        zoomValue *= scroll > 0 ? 1 + scrollSensitivity : 1 - scrollSensitivity;
+        zoomValue = Clamp(zoomValue, minZoomValue, maxZoomValue);
+
+        transform.localScale = originalScale * zoomValue;
     }
 
     private void OnDrawGizmosSelected()
