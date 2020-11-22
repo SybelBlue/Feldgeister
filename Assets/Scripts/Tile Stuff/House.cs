cd1ae21d6f8be02d4e6804c82b5718d6f5365202
L Caraco <lbc.dna@gmail.com>
2020-11-21 22:36:40 -0600

    added example onclick

--- Assets/Scripts/Tile Stuff/House.cs
+++ Assets/Scripts/Tile Stuff/House.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class House : Building
 {
     public Character character;
+
     public static House AddTo(GameObject gameObject, string name, Character character, RectInt region)
     {
         House house = gameObject.AddComponent<House>() as House;
@@ -11,5 +12,10 @@ public class House : Building
         house.character = character;
         house.region = region;
         return house;
+    }
+
+    public override void OnClick()
+    {
+        print($"Clicked the {character}'s house!");
     }
 }
