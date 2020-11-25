b59ebed09cc3e9649b2ebea13536ec1dea196d72
L Caraco <lbc.dna@gmail.com>
2020-11-25 12:51:30 -0600

    more cleanup

--- Assets/Scripts/Tile Stuff/House.cs
+++ Assets/Scripts/Tile Stuff/House.cs
@@ -7,36 +7,24 @@ public class House : Building
     public CharacterClass character;
 
     public List<BuildingDefense> defenses;
+
     public int defenseLevel
     {
         get => defenses.Select(defenseItem => defenseItem.value).Sum();
-        set => defenses = 
-            defenses.Aggregate(
-                (list: new List<BuildingDefense>(), remaining: value), 
-                (tuple, defenseItem) => {
-                    if (tuple.remaining <= 0) {
-                        // if there are no remaining points,
-                        // don't add any more defenses
-                        return tuple;
-                    }
-
-                    // add the next item to the new list
-                    tuple.list.Add(defenseItem);
-
-                    if (defenseItem.value > tuple.remaining) 
-                    {
-                        // if there isn't enough remaining, 
-                        // decrease the value of the last item
-                        defenseItem.value = tuple.remaining;
-                        defenseItem.damaged = true; // set damaged flag
-                    }
-                    
-                    // subtract the value of the defenseItem
-                    tuple.remaining -= defenseItem.value;
-                    
-                    return tuple;
-                })
-                .list;
+        set
+        {
+            int remainingPoints = value;
+            defenses = defenses.TakeWhile(defenseItem => {
+                if (remainingPoints <= 0) return false;
+                if (defenseItem.value > remainingPoints) 
+                {
+                    defenseItem.value = remainingPoints;
+                    defenseItem.damaged = true; // set damaged flag
+                }
+                remainingPoints -= defenseItem.value;
+                return true;
+            }).ToList();
+        }
     }
 
     public static House AddTo(GameObject gameObject, string name, CharacterClass character, RectInt region)
