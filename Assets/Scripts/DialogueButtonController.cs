97e85a16fe88607f57851bf9952ab3563bd781f0
L Caraco <lbc.dna@gmail.com>
2020-12-01 12:40:30 -0600

    made a comprehensive button style

---
+++ Assets/Scripts/DialogueButtonController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DialogueButtonController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    
+    [ReadOnly, SerializeField]
+    private Text text;
+
+    void OnValidate()
+    {
+        text = transform.GetChild(0).GetComponent<Text>();
+    }
+    
+    public void OnPointerEnter(PointerEventData data)
+    {
+        if (!text.text.StartsWith("> ") && !text.text.EndsWith(" <"))
+        {
+            text.text = $"> {text.text} <";
+        }
+    }
+    
+    public void OnPointerExit(PointerEventData data)
+    { 
+        if (text.text.StartsWith("> ") && text.text.EndsWith(" <"))
+        {
+            text.text = text.text.Substring(2, text.text.Length - 4);
+        }
+    }
+}
