b7fd2c22c8ae36cf6616079a12a136fe4cdb1614
L Caraco <lbc.dna@gmail.com>
2020-12-04 10:30:08 -0600

    split enums file finally

---
+++ Assets/Scripts/Character/CharacterClass.cs
@@ -0,0 +1,11 @@
+/// <summary> An enum to represent all the character classes </summary>
+[System.Serializable]
+public enum CharacterClass
+{
+    Miner,
+    Blacksmith,
+    Witch,
+    Watcher,
+    Farmer,
+    Mayor,
+}
---
+++ Assets/Scripts/Character/HungerLevel.cs
@@ -0,0 +1,9 @@
+/// <summary> An enum to represent character hunger levels </summary>
+[System.Serializable]
+public enum HungerLevel
+{
+    Starving,
+    Hungry,
+    Sated,
+    Full,
+}
