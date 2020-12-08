e1410a69ead3fc4df70a002a6f402da0abf0d317
Katia Sievert <ksievert@macalester.edu>
2020-12-08 11:57:42 -0600

    some basic methods for DefenseList and ResourceList

--- Assets/Scripts/DefenseList.cs
+++ Assets/Scripts/DefenseList.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class DefenseList : MonoBehaviour
 {
-    public void toggleDefenses()
+    public TMP_Text blacksmith;
+    public Character blacksmithChar;
+    public TMP_Text witch;
+    public Character witchChar;
+    public TMP_Text miner;
+    public Character minerChar;
+    public TMP_Text farmer;
+    public Character farmerChar;
+
+    public TMP_Text watcher;
+    public Character watcherChar;
+    public void toggleDisplay()
     {
         DefenseList.setActive(!activeSelf);
+    }
+    public void UpdateAll(){
+        blacksmith.text = "Blacksmith: " + blacksmithChar.house.defenseLevel;
+        witch.text = "Witch: " + witchChar.house.defenseLevel;
+        miner.text = "Miner: " + minerChar.house.defenseLevel;
+        farmer.text = "Farmer: " + farmerChar.house.defenseLevel;
+        watcher.text = "Watcher: " + watcherChar.house.defenseLevel;
     }
 }
--- Assets/Scripts/ResourceList.cs
+++ Assets/Scripts/ResourceList.cs
@@ -4,8 +4,44 @@ using UnityEngine;
 
 public class ResourceList : MonoBehaviour
 {
-    public void toggleResources()
+    public int salt = 0;
+    public TMP_Text minerText;
+    public int weapons = 0;
+    public TMP_Text blacksmithText;
+    public int enchantment = 0;
+    public TMP_Text witchText;
+    public int rations = 2;
+    public TMP_Text farmerText;
+    public void ToggleDisplay()
     {
         this.setActive(!activeSelf);
     }
+    public void AddSalt(int moreSalt)
+    {
+        salt += moreSalt;
+        minerText.text = "Salt: " + salt;
+    }
+    public void AddWeapons(int moreWeapons)
+    {
+        weapons += moreWeapons;
+        blacksmithText.text = "Weapons: " + weapons;
+    }
+    public void AddEnchantment()
+    {
+        witchText.text = "Enchantments: " + (++enchantment);
+    }
+    public void AddRations(int moreRations)
+    {
+        rations += moreRations;
+        farmerText.text = "Rations: " + rations;
+    }
+
+    public void ResetAll()
+    {
+        AddSalt(-salt);
+        AddWeapons(-weapons);
+        AddEnchantment(-enchantment);
+        AddRations((-rations + 2));
+    }
+
 }
