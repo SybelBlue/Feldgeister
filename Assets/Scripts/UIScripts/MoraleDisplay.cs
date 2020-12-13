a96feb9190790351fe45c9c5adc12768fc5e4f49
Katia Sievert <ksievert@macalester.edu>
2020-12-13 16:41:05 -0600

    Morale Display script and minor intro scene changes

--- Assets/Scripts/UIScripts/MoraleDisplay.cs
+++ Assets/Scripts/UIScripts/MoraleDisplay.cs
@@ -6,24 +6,30 @@ using TMPro;
 public class MoraleDisplay : MonoBehaviour
 {
     public TMP_Text morale;
-    public AudioSource moraleUp;
-    public AudioSource moraleDown;
+    public AudioSource audioSource;
+    public AudioClip moraleUp;
+    public AudioClip moraleDown;
     
     public void IncreaseMorale()
     {
         //from https://gamedevbeginner.com/how-to-play-audio-in-unity-with-examples/#play, 
         //https://answers.unity.com/questions/1327656/how-to-change-the-color-of-the-textmeshpro-gui-com.html,
-        //https://answers.unity.com/questions/796881/c-how-can-i-let-something-happen-after-a-small-del.html
-        morale.text = "+ morale";
         morale.color = new Color(50,150,50,255); //green
-        moraleUp.Play();
-        //yield return new WaitForSeconds(2);
-        
+        morale.text = "+ morale";
+        audioSource.PlayOneShot(moraleUp);
+        while(audioSource.isPlaying){
+            print("still playing");
+        }
+        morale.text = "";
     }
     public void DecreaseMorale() 
     {
-        morale.text = "- morale";
         morale.color = new Color(150,50,50,255); //red
-        moraleDown.PlayOneShot()
+        morale.text = "- morale";
+        audioSource.PlayOneShot(moraleDown);
+        while(audioSource.isPlaying){
+            print("still playing");
+        }
+        morale.text = "";
     }
 }
