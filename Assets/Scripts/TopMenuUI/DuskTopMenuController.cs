3f49e9eb0840e18faf031299fed78db426590f10
Katia Sievert <ksievert@macalester.edu>
2020-12-16 16:37:28 -0600

    dusktopmenucontroller, change to top menu prefab

---
+++ Assets/Scripts/TopMenuUI/DuskTopMenuController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+
+public class DuskTopMenuController : MonoBehaviour
+{
+    public TMP_Text foodRemaining;
+    public UnityEvent hideResources;
+    public UnityEvent hideDefenses;
+    public UnityEvent changeToDusk;
+   
+   //reduces food by one, should be called after speaking with a character
+    public void ReduceFood()
+    {
+        
+    }
+//resets food remaining text (needs to eventually update food variable as well)
+    public void ResetFood()
+    {
+        
+    }
+    //hides resource and defense lists but keeps top menu
+    public void HideLists()
+    {
+        hideResources.Invoke();
+        hideDefenses.Invoke();
+    }
+    //hides both the lists and the top menu itself
+    public void HideAll()
+    {
+        this.gameObject.SetActive(false);
+    }
+    //shows just the top menu and not the lists
+    public void ShowTop()
+    {
+        this.gameObject.SetActive(true);
+        hideResources.Invoke();
+        hideDefenses.Invoke();
+    }
+}
